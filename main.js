77379defe5e463c9fe24ab2de36020ed59d4ab92
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-04-16 21:24:51 +0300

    js contactform

--- main.js
+++ main.js
@@ -4,13 +4,43 @@ let sections = document.querySelectorAll("section");
 
 for(button of showContentBtns){
     button.onclick = (e)=>{
+        // for(sec of sections){
+        //     sec.classList.replace("activeSec","inactiveSec");
+        // }
+
         let secId = e.target.dataset.id;
-        document.querySelector("#"+secId).classList.toggle("activeSec");
+
+        if(document.querySelector("#"+secId).classList.contains("activeSec")){
+            document.querySelector("#"+secId).classList.replace("activeSec","inactiveSec");
+        }else{
+            document.querySelector("#"+secId).classList.toggle("activeSec");
+        }
+       
     }
 }
 
 
 
 
-console.log(showContentBtns[0]);
-console.log(sections);
+// console.log(showContentBtns[0]);
+// console.log(sections);
+
+
+
+// Dialog
+
+
+let dialog = document.querySelector("#contactFormDialog");
+let dialogOpenBtn = document.querySelector(".contactBtn");
+let dialogCloseBtn = document.querySelector(".dialogCloseBtn");
+
+dialogOpenBtn.onclick = ()=>{
+    dialog.showModal();
+}
+
+dialogCloseBtn.onclick = ()=>{
+    dialog.close();
+}
+
+
+
