9adaebe20fb1015facfae15761b43b9ffe878ea3
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-04-23 21:33:05 +0300

    carousel.js

--- carousel.js
+++ carousel.js
@@ -2,27 +2,27 @@ const datorkursi = [
     {
       nosaukums: "Web izstrādes pamati",
       apraksts: "Šis kurss iepazīstinās jūs ar HTML, CSS un JavaScript pamatiem, ļaujot jums izveidot vienkāršas tīmekļa lapas un izprast tīmekļa izstrādes pamatus.",
-      attels: "https://brainscloud.lv/"
+      attels: "https://picsum.photos/1000/300"
     },
     {
       nosaukums: "Programmēšana ar Python",
       apraksts: "Apgūstiet vienu no populārākajām programmēšanas valodām pasaulē. Šis kurss aptver Python pamatus, datu struktūras, funkcijas un moduļus.",
-      attels: "https://proghelp.lv/programmesana/python"
+      attels: "https://picsum.photos/1000/300"
     },
     {
       nosaukums: "Datu analīze ar R",
       apraksts: "Iemācieties analizēt un vizualizēt datus, izmantojot R programmēšanas valodu. Kurss ietver datu importēšanu, tīrīšanu, analīzi un grafiku veidošanu.",
-      attels: "https://bookdown.org/delferts/PBB_gramata/grafiskas-iespejas-programma-r.html"
+      attels: "https://picsum.photos/1000/300"
     },
     {
       nosaukums: "Mobilā lietotņu izstrāde ar React Native",
       apraksts: "Šis kurss iepazīstinās jūs ar React Native, ļaujot jums izveidot mobilās lietotnes gan iOS, gan Android platformām, izmantojot JavaScript un React.",
-      attels: "https://www.freecodecamp.org/news/mobile-app-development-with-react-native/"
+      attels: "https://picsum.photos/1000/300"
     },
     {
       nosaukums: "Mākslīgais intelekts un mašīnmācīšanās",
       apraksts: "Iepazīstieties ar mākslīgā intelekta un mašīnmācīšanās pamatiem. Kurss aptver uzraudzīto un neuzraudzīto mācīšanos, neironu tīklus un praktiskus piemērus.",
-      attels: "https://unite.ai/lv/Ma%C5%A1%C4%ABnm%C4%81c%C4%ABbas-un-m%C4%81ksl%C4%ABg%C4%81-intelekta-galven%C4%81s-at%C5%A1%C4%B7ir%C4%ABbas/"
+      attels: "https://picsum.photos/1000/300"
     }
   ];
   
