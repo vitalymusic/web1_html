fcf96f300a3f8dea1ed400b0ccec71f71c295960
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-04-23 22:43:10 +0300

    Carousel with images

--- carousel.js
+++ carousel.js
@@ -2,27 +2,27 @@ const datorkursi = [
     {
       nosaukums: "Web izstrādes pamati",
       apraksts: "Šis kurss iepazīstinās jūs ar HTML, CSS un JavaScript pamatiem, ļaujot jums izveidot vienkāršas tīmekļa lapas un izprast tīmekļa izstrādes pamatus.",
-      attels: "https://picsum.photos/1000/300"
+      attels: "./img/slide1.jpg"
     },
     {
       nosaukums: "Programmēšana ar Python",
       apraksts: "Apgūstiet vienu no populārākajām programmēšanas valodām pasaulē. Šis kurss aptver Python pamatus, datu struktūras, funkcijas un moduļus.",
-      attels: "https://picsum.photos/1000/300"
+      attels: "./img/slide2.jpg"
     },
     {
       nosaukums: "Datu analīze ar R",
       apraksts: "Iemācieties analizēt un vizualizēt datus, izmantojot R programmēšanas valodu. Kurss ietver datu importēšanu, tīrīšanu, analīzi un grafiku veidošanu.",
-      attels: "https://picsum.photos/1000/300"
+      attels: "./img/slide3.jpg"
     },
     {
       nosaukums: "Mobilā lietotņu izstrāde ar React Native",
       apraksts: "Šis kurss iepazīstinās jūs ar React Native, ļaujot jums izveidot mobilās lietotnes gan iOS, gan Android platformām, izmantojot JavaScript un React.",
-      attels: "https://picsum.photos/1000/300"
+      attels: "./img/slide4.jpg"
     },
     {
       nosaukums: "Mākslīgais intelekts un mašīnmācīšanās",
       apraksts: "Iepazīstieties ar mākslīgā intelekta un mašīnmācīšanās pamatiem. Kurss aptver uzraudzīto un neuzraudzīto mācīšanos, neironu tīklus un praktiskus piemērus.",
-      attels: "https://picsum.photos/1000/300"
+      attels: "./img/slide5.jpg"
     }
   ];
   
@@ -35,14 +35,13 @@ let rightbtn = document.querySelector('.slider_right_btn');
 
 
 function showSlide(number){
-    slidesDiv.innerHTML = `
+      slidesDiv.innerHTML = `
          <div class="slide">
             <img src="${datorkursi[number].attels}" alt="">
             <h3>${datorkursi[number].nosaukums}</h3>
             <p>${datorkursi[number].apraksts}</p>
             <button>Pieteikties</button>
         </div> `;
-
 }
 
 
@@ -75,6 +74,7 @@ rightbtn.onclick = (event)=>{
 setInterval(()=>{
     rightbtn.click();
 },2000);
+
 
 
 
