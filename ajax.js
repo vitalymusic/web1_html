8b8eeb1cf80f70c7d91f703372ea6b19cb97fda0
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-04-24 20:53:11 +0300

    fetch1

--- ajax.js
+++ ajax.js
@@ -8,7 +8,7 @@
          ieliktTabula(dati);
     })
     .catch((error)=>{
-        alert(error);
+        // alert(error);
     })
     .finally(()=>{
         console.log("Dati ielādēti");
@@ -16,6 +16,32 @@
 
 
     function ieliktTabula(dati){
+        let table="";
+
+        for(rinda of dati.tags){
+            let attr = "";
+            for(atributs in rinda.attributes){
+                attr += `${atributs} : ${rinda.attributes[atributs]}<br>` 
+            }
+
+            table+=`
+                <tr>
+                    <td>${rinda.tag}</td>
+                    <td>${rinda.description}</td>
+                    <td>${attr}</td>
+                </tr>
+            `;
+        }
+
+
+
+
+
+        document.querySelector("table").innerHTML = table;
+        
+        
+
+
         console.log(dati)
     }
 
