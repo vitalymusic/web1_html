140733e17b47b215d50ff339ded37d9b8c575d54
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-04-24 19:04:33 +0300

    preloadImage

--- carousel.js
+++ carousel.js
@@ -1,37 +1,46 @@
+function preloadImage(url){
+  let image = new Image();
+  image.src = url;
+  return image;
+}
+
 const datorkursi = [
     {
       nosaukums: "Web izstrādes pamati",
       apraksts: "Šis kurss iepazīstinās jūs ar HTML, CSS un JavaScript pamatiem, ļaujot jums izveidot vienkāršas tīmekļa lapas un izprast tīmekļa izstrādes pamatus.",
-      attels: "./img/slide1.jpg"
+      attels: preloadImage("./img/slide1.jpg").src
     },
     {
       nosaukums: "Programmēšana ar Python",
       apraksts: "Apgūstiet vienu no populārākajām programmēšanas valodām pasaulē. Šis kurss aptver Python pamatus, datu struktūras, funkcijas un moduļus.",
-      attels: "./img/slide2.jpg"
+      attels: preloadImage("./img/slide2.jpg").src
     },
     {
       nosaukums: "Datu analīze ar R",
       apraksts: "Iemācieties analizēt un vizualizēt datus, izmantojot R programmēšanas valodu. Kurss ietver datu importēšanu, tīrīšanu, analīzi un grafiku veidošanu.",
-      attels: "./img/slide3.jpg"
+      attels: preloadImage("./img/slide3.jpg").src
     },
     {
       nosaukums: "Mobilā lietotņu izstrāde ar React Native",
       apraksts: "Šis kurss iepazīstinās jūs ar React Native, ļaujot jums izveidot mobilās lietotnes gan iOS, gan Android platformām, izmantojot JavaScript un React.",
-      attels: "./img/slide4.jpg"
+      attels: preloadImage("./img/slide4.jpg").src
     },
     {
       nosaukums: "Mākslīgais intelekts un mašīnmācīšanās",
       apraksts: "Iepazīstieties ar mākslīgā intelekta un mašīnmācīšanās pamatiem. Kurss aptver uzraudzīto un neuzraudzīto mācīšanos, neironu tīklus un praktiskus piemērus.",
-      attels: "./img/slide5.jpg"
+      attels: preloadImage("./img/slide5.jpg").src
     }
   ];
   
+
+
+
 
 let slideNumber = 0;
 let slidesDiv = document.querySelector('.slides');
 let leftbtn = document.querySelector('.slider_left_btn');
 let rightbtn = document.querySelector('.slider_right_btn');
-
+let time = 800;
 
 
 function showSlide(number){
@@ -62,18 +71,27 @@ leftbtn.onclick = (event)=>{
 
 rightbtn.onclick = (event)=>{
     event.preventDefault();
+    slidesDiv.classList.remove('fade-in');
+    slidesDiv.classList.add('fade-out');
+
+  setTimeout(()=>{
     slideNumber++;
     if(slideNumber > datorkursi.length -1){
         slideNumber=0;
     }
-
     showSlide(slideNumber);
+  },time);
+
+  setTimeout(()=>{
+    slidesDiv.classList.replace('fade-out','fade-in');
+  },time);
+
     console.log(slideNumber);
 }
 
-setInterval(()=>{
-    rightbtn.click();
-},2000);
+// setInterval(()=>{
+//     rightbtn.click();
+// },2000);
 
 
 
