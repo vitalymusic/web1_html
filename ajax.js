438413ff4d7f30670a756f42d2adf1245aa2aed2
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-04-30 18:19:37 +0300

    Weather1

--- ajax.js
+++ ajax.js
@@ -47,6 +47,9 @@
 
     // Kas ir API
 
-    // Meteo API vaicajums
-    let href = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code&hourly=temperature_2m&timezone=Europe%2FLondon&forecast_days=7";
+    
+
+
+
+
 
---
+++ forecast.js
@@ -0,0 +1,11 @@
+// Meteo API vaicajums
+let href = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code&hourly=temperature_2m&timezone=Europe%2FRiga&forecast_days=7";
+
+
+
+
+
+
+
+
+
