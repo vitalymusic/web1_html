a3a16b7275aefa221112240ba2bd7681dc9653f6
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-04-30 19:58:26 +0300

    laika prognoze

--- forecast.js
+++ forecast.js
@@ -1,18 +1,32 @@
-let input = +document.querySelector("input");
+let tests = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code&hourly=temperature_2m&timezone=Europe%2FRiga&forecast_days=3";
+
+let ievadesLauks = document.querySelector(".input input");
 let button = document.querySelector(".btn1");
 
+
+let pilsetas = {
+    "Rīga":"latitude=52.52&longitude=13.41",
+    "Jelgava":"latitude=56.65&longitude=23.7128",
+    "Liepāja":"latitude=56.5047&longitude=21.0109",
+    "Saldus":"latitude=56.6636&longitude=22.4881",
+    "Kuldīga":"latitude=56.974&longitude=21.9572"
+}
+
+for( pilseta in pilsetas){
+    console.log(pilseta,pilsētas[pilseta]);
+}
+
 button.onclick = ()=>{
-    if(input.value > 0){
-        getWeather(days);
+    if(ievadesLauks.value > 0){
+        getWeather(ievadesLauks.value);
+    }else{
+        alert("Ievadiet vērtību")
     }
- 
 }
 
 let div = document.querySelector(".forecast");
 async function getWeather(days){
     let href = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code&hourly=temperature_2m&timezone=Europe%2FRiga&forecast_days=${days}`;
-
-
 
     let atbilde = await fetch(href);
     let dati = await atbilde.json();
@@ -20,7 +34,6 @@ async function getWeather(days){
 }
 
 function showTable(data){
-
     let out = `
  <table class="table">
     <tr>
